0a78300002ffcb0a29002e63a9408c74b987c9d0
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-07-27 10:40:53 +0530

    Made a search function which is shared between the google and the forum search

--- cogs/forum.py
+++ cogs/forum.py
@@ -11,33 +11,31 @@ class Forum(commands.Cog):
     @commands.command(aliases=["forums", "search"])
     @commands.cooldown(1, 45, commands.BucketType.user)
     async def forum(self, ctx, *srch):  
-        param = " ".join(srch)
-        param += " site:forum.xda-developers.com"
-    
-        embed = discord.Embed(title = "__Here are the results for your search:__", color = 0x301c24)
-        search_url = list(googlesearch.search(param, num_results=10))
 
-        del search_url[5:]
-    
-        for links, n in zip(search_url, num):
-            embed.add_field(name = f"Link #{n}:", value = links, inline=False)
-        embed.set_footer(text = "Happy modding :)")
+        embed = self.search(srch, xda=True)
         await ctx.send(embed = embed)
         
     @commands.command()
     @commands.cooldown(1, 45, commands.BucketType.user)
     async def google(self, ctx, *srch):  
-        param = " ".join(srch)
-    
-        embed = discord.Embed(title = "__Here are the results for your search:__", color = 0x301c24)
-        search_url = list(googlesearch.search(param, num_results=10))
 
-        del search_url[5:]
-    
+        embed = self.search(srch)
+
+        await ctx.send(embed = embed)
+
+    def search(self, srch=[], xda=False):
+        param = " ".join(srch)
+        if xda:
+            param += " site:forum.xda-developers.com"
+
+        embed = discord.Embed(title = "__Here are the results for your search:__", color = 0x301c24)
+        search_url = list(googlesearch.search(param, num_results=5))
+
         for links, n in zip(search_url, num):
             embed.add_field(name = f"Link #{n}:", value = links, inline=False)
         embed.set_footer(text = "Hope you found what you're looking for")
-        await ctx.send(embed = embed)
+
+        return embed
         
 def setup(client):
     client.add_cog(Forum(client))
