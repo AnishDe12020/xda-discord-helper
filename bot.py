b0d1ba6cb2185a797e8f04f60ade17b036028032
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-07-27 09:57:44 +0530

    Shifted things to a main function in bot.py

--- bot.py
+++ bot.py
@@ -5,35 +5,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-activity = discord.Streaming(name="xda/help", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ/")
-client = commands.Bot(command_prefix = "axda/", activity = activity, intents=discord.Intents.all())
-client.remove_command("help")
-slash = SlashCommand(client, sync_commands=True)
 
-guild_ids = os.environ["SLASH_GUILD_IDS"]
-guild_ids = guild_ids.split(" ")
-guild_ids = [int(guild_id) for guild_id in guild_ids]
+def main():
+    activity = discord.Streaming(name="xda/help", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ/")
+    client = commands.Bot(command_prefix = "axda/", activity = activity, intents=discord.Intents.all())
+    client.remove_command("help")
+    slash = SlashCommand(client, sync_commands=True)
 
-# def main():
+    guild_ids = os.environ["SLASH_GUILD_IDS"]
+    guild_ids = guild_ids.split(" ")
+    guild_ids = [int(guild_id) for guild_id in guild_ids]
 
-
-
-###########################################################
-"""Ping commands"""
-@slash.slash(name="ping", description="Just a test command",
-         guild_ids=guild_ids)
-async def _ping(ctx): 
-    await ctx.send(f"Pong! {round(client.latency*1000)}ms")
+    """Ping commands"""
+    @slash.slash(name="ping", description="Just a test command",
+            guild_ids=guild_ids)
+    async def _ping(ctx): 
+        await ctx.send(f"Pong! {round(client.latency*1000)}ms")
+            
+    @client.command()
+    async def ping(ctx):
+        await ctx.send(f"Pong! {round(client.latency*1000)}ms") 
         
-@client.command()
-async def ping(ctx):
-    await ctx.send(f"Pong! {round(client.latency*1000)}ms")
-###########################################################    
+    for file in os.listdir('./cogs'):
+        if file.endswith(".py"):
+            client.load_extension(f"cogs.{file[:-3]}")
         
         
-for file in os.listdir('./cogs'):
-    if file.endswith(".py"):
-        client.load_extension(f"cogs.{file[:-3]}")
-    
-    
-client.run(os.environ["BOT_TOKEN"])
+    client.run(os.environ["BOT_TOKEN"])
+
+
+if __name__ == "__main__":
+    main()
